1f6edfea36fceee46a318f7343b969f4eff9964a
root <root@stan>
2023-09-20 18:55:28 +0300

    user.py

--- models/user.py
+++ models/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""
+module for class User
+"""
 from models.base_model import BaseModel, Base
 from models import storage_type
 from sqlalchemy import Column, String
@@ -7,7 +9,15 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """
+    Class User that imports from BaseModel
+
+    Attributes:
+    email: email of User
+    password: password of User
+    first_name: first name of User
+    last_name: last name of User
+    """
     __tablename__ = 'users'
     if storage_type == 'db':
         email = Column(String(128), nullable=False)
