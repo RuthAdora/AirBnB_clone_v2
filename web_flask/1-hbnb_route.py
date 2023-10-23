76956919135f234ac39b5e6b095bb0fd4341f3b3
RuthAdora <adoraruth01@hmail.com>
2023-10-23 19:51:24 +0300

    airbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Script that runs an app with Flask framework"""
+"""Flask framework"""
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Function called with / route"""
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """return hello HBNB"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Function called with /hbnb route"""
-    return 'HBNB'
+    """return HBNB"""
+    return "HBNB"
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+"""Flask framework"""
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Function called with / route """
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """return hello HBNB"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """ Function called with /hbnb route """
-    return 'HBNB'
+    """return HBNB"""
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """ Function called with /c/<text> route """
-    return 'C %s' % text.replace('_', ' ')
+def text(text):
+    """return text given"""
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is_cool'})
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """ Function called with /python/<text> route """
-    if text is not 'is cool':
-        text = text.replace('_', ' ')
-    return 'Python %s' % text
+def display(text):
+    """display “Python ”, followed by the value of the text"""
+    return "Python {}".format(text.replace("_", " "))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
